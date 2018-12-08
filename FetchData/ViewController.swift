bd8be52aeec597202593171c4781c97d230c1253
danial eghbali <danialeghbali@danials-Mac.local>
2018-12-08 23:24:28 +0000

    Initial Commit

---
+++ FetchData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FetchData
+//
+//  Created by danial eghbali on 12/8/18.
+//  Copyright © 2018 danial eghbali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
